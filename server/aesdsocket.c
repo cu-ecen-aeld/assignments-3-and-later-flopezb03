41d569f867ce656f38b9ac32c88325d26f130cee
flopezb03 <f.lopezberrocal@gmail.com>
2025-11-05 17:48:17 +0100

    Assignment 6 Part 1 Refactoring

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -14,11 +14,16 @@
 #include "slist_foreach_safe.h"
 #include <pthread.h>
 #include <time.h>
-
 #include <stdlib.h>
+
+
 
 #define VARFILE_PATH "/var/tmp/aesdsocketdata"
 #define BUFFER_SIZE 1024
+
+
+
+
 
 int server_fd = -1;
 
@@ -32,8 +37,125 @@ struct entry {
 
     SLIST_ENTRY(entry) entries;
 };
+
 pthread_mutex_t mutex;
+
 int exit_signal = 0;
+
+
+
+
+void closeall();
+
+void close_signal_handler();
+void timer_signal_handler();
+void set_signals();
+void set_timer();
+
+void set_daemon_mode();
+void init_server_socket();
+
+void* thread_task(void* arg);
+int socket2file(int client_fd);
+int file2socket(int client_fd);
+
+
+
+
+
+
+int main(int argc, char** argv){
+    int daemon_mode = 0;
+
+    struct sockaddr_in client_sockaddr;
+    socklen_t client_addrlen;
+    char ip4[INET_ADDRSTRLEN];
+
+    struct entry *node_iterator, *node_iterator_tmp;
+
+
+
+    // Init vars
+    if (argc == 2 && strcmp(argv[1], "-d") == 0)
+        daemon_mode = 1;
+
+    pthread_mutex_init(&mutex,NULL);
+
+    openlog(NULL, 0, LOG_USER);
+    // Set sigaction
+    set_signals();
+
+
+
+    // Socket init
+    init_server_socket();
+    // Set daemon mode
+    if(daemon_mode)
+        set_daemon_mode();
+
+
+
+    // Set timer
+    set_timer();
+
+    // Server starts listen
+    listen(server_fd, 1024);
+
+    while(1){
+
+        // Delete finished threads
+        SLIST_FOREACH_SAFE(node_iterator, &head, entries, node_iterator_tmp) {
+            if (node_iterator->end) {
+                pthread_join(node_iterator->t, NULL);
+
+                close(node_iterator->client_fd);
+                SLIST_REMOVE(&head, node_iterator, entry, entries);
+                syslog(LOG_INFO, "Closed connection from %s", node_iterator->client_ip4);
+
+                free(node_iterator);
+            }
+        }
+
+        // Accept new client
+        client_addrlen = sizeof(client_sockaddr);
+        int new_client_fd = accept(server_fd, (struct sockaddr*)&client_sockaddr, &client_addrlen);
+        if(new_client_fd == -1){
+            if (exit_signal) 
+                break;
+            continue;
+        }
+
+        inet_ntop(AF_INET, &client_sockaddr.sin_addr, ip4, INET_ADDRSTRLEN);
+        syslog(LOG_INFO, "Accepted connection from %s", ip4);
+
+
+        // Create list node for the client
+        struct entry* new_node = malloc(sizeof(struct entry));
+        new_node->client_fd = new_client_fd;
+        strcpy(new_node->client_ip4, ip4);
+        new_node->end = 0;
+
+        // Init thread and insert node in the list
+        if(pthread_create(&new_node->t, NULL, thread_task, new_node) == 0)
+            SLIST_INSERT_HEAD(&head, new_node, entries);
+
+    }
+
+    SLIST_FOREACH_SAFE(node_iterator, &head, entries, node_iterator_tmp){
+        pthread_join(node_iterator->t, NULL);
+
+        close(node_iterator->client_fd);
+        SLIST_REMOVE(&head, node_iterator, entry, entries);
+        syslog(LOG_INFO, "Closed connection from %s", node_iterator->client_ip4);
+
+        free(node_iterator);
+    }
+    closeall();
+    
+    exit(EXIT_SUCCESS);
+}
+
+
 
 void closeall(){
     if(server_fd != -1)
@@ -144,98 +266,6 @@ void init_server_socket(){
         exit(EXIT_FAILURE);
     }
     freeaddrinfo(server_addrinfo);
-}
-
-void* thread_task(void* arg);
-int socket2file(int client_fd);
-int file2socket(int client_fd);
-
-int main(int argc, char** argv){
-    struct sockaddr_in client_sockaddr;
-    socklen_t client_addrlen;
-    char ip4[INET_ADDRSTRLEN];
-    int daemon_mode = 0;
-    struct entry *node_iterator, *node_iterator_tmp;
-
-
-    // Init vars
-    if (argc == 2 && strcmp(argv[1], "-d") == 0)
-        daemon_mode = 1;
-
-    pthread_mutex_init(&mutex,NULL);
-
-    openlog(NULL, 0, LOG_USER);
-    // Set sigaction
-    set_signals();
-
-
-
-    // Socket init
-    init_server_socket();
-    // Set daemon mode
-    if(daemon_mode)
-        set_daemon_mode();
-
-
-
-    // Set timer
-    set_timer();
-
-    // Server starts listen
-    listen(server_fd, 1024);
-
-    while(1){
-
-        // Delete finished threads
-        SLIST_FOREACH_SAFE(node_iterator, &head, entries, node_iterator_tmp) {
-            if (node_iterator->end) {
-                pthread_join(node_iterator->t, NULL);
-
-                close(node_iterator->client_fd);
-                SLIST_REMOVE(&head, node_iterator, entry, entries);
-                syslog(LOG_INFO, "Closed connection from %s", node_iterator->client_ip4);
-
-                free(node_iterator);
-            }
-        }
-
-        // Accept new client
-        client_addrlen = sizeof(client_sockaddr);
-        int new_client_fd = accept(server_fd, (struct sockaddr*)&client_sockaddr, &client_addrlen);
-        if(new_client_fd == -1){
-            if (exit_signal) 
-                break;
-            continue;
-        }
-
-        inet_ntop(AF_INET, &client_sockaddr.sin_addr, ip4, INET_ADDRSTRLEN);
-        syslog(LOG_INFO, "Accepted connection from %s", ip4);
-
-
-        // Create list node for the client
-        struct entry* new_node = malloc(sizeof(struct entry));
-        new_node->client_fd = new_client_fd;
-        strcpy(new_node->client_ip4, ip4);
-        new_node->end = 0;
-
-        // Init thread and insert node in the list
-        if(pthread_create(&new_node->t, NULL, thread_task, new_node) == 0)
-            SLIST_INSERT_HEAD(&head, new_node, entries);
-
-    }
-
-    SLIST_FOREACH_SAFE(node_iterator, &head, entries, node_iterator_tmp){
-        pthread_join(node_iterator->t, NULL);
-
-        close(node_iterator->client_fd);
-        SLIST_REMOVE(&head, node_iterator, entry, entries);
-        syslog(LOG_INFO, "Closed connection from %s", node_iterator->client_ip4);
-
-        free(node_iterator);
-    }
-    closeall();
-    
-    exit(EXIT_SUCCESS);
 }
 
 void* thread_task(void* arg){
